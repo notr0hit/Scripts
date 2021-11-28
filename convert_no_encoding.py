60cf0ebc65d07ff1efca11094f9baa530ecfd29d
Rohit Khare <rohit97528@gmail.com>
2021-11-28 16:51:20 +0530

    m

--- convert_no_encoding.py
+++ convert_no_encoding.py
@@ -1,5 +1,3 @@
-# ffmpeg -i input.xyz -map 0 -c copy output.mp4
-
 import os
 import sys
 directory = os.getcwd()
@@ -10,7 +8,7 @@ output_extension = '.'+sys.argv[2];
 for filename in os.listdir(directory):
     if filename.endswith(input_extension):
         file_name = os.path.splitext(filename)[0]
-        # ffmpeg -i input -map 0 -c copy output.mp4
-        os.system(f'ffmpeg -i {filename} -map 0 -c copy {file_name + output_extension}')
+        # ffmpeg -i input -c copy output.mp4
+        os.system(f'ffmpeg -i ".\\{filename}" -c copy ".\\{file_name + output_extension}"')
     else:
         continue
