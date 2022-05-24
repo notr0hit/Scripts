1f9fa74a2627481e0f60e797bc0c0032fba4f510
Rohit Khare <rohit97528@gmail.com>
2022-05-24 18:30:35 +0530

    -

--- cpp_compile_run.py
+++ cpp_compile_run.py
@@ -8,25 +8,31 @@ script_path = 'F:\\Scripts\\cpp_compile_run.py'
 dir = os.getcwd()
 
 # filename from argument
-file = sys.argv[1];
+file = sys.argv[1]
 
 if(file[0:2] == "./" or file[0:2] == ".\\"):
     file = file[2:]
 
-if file.endswith('.cpp') == False:
+if not file.endswith('.cpp'):
     file = file + '.cpp'
 
 file_name = os.path.splitext(file)[0]
 
+if len(file_name) == 1:
+    file_name = file_name.lower()
+    file = file_name + 'cpp'
+
 # compiling and running
 def compile_run():
-    os.system(f'g++ -std=c++17 -Wall -Werror -O2 -DROHIT {file} -o {file_name}')
-    run()
+    if os.system(f'g++ -std=c++17 -Wall -DLOCAL {file} -o {file_name}.exe') == 0:
+        run()
+    else:
+        print("Compilation Failed!")
 
 # executing
 def run():
-    os.system(f'{dir}/{file_name}')
-    print()
+    os.system(f'{dir}/{file_name}.exe')
+    os.system
 
 def choice_is_bad(choice):
     return choice != 'c' and choice != 'r' and choice != 'q'
@@ -36,9 +42,9 @@ print()
 compile_run()
 
 while True:
-    choice = input(f'Compile, Run again or Quit? (c / r / q) : ').lower()
+    choice = input(f'\nCompile, Run again or Quit? (c / r / q) : ').lower()
     while choice_is_bad(choice):
-        choice = input(f'Invalid Input. Enter again : ')
+        choice = input(f'\nInvalid Input. Enter again : ')
     print()
     
     if choice == 'c':
